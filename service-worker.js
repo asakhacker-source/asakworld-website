acc8fe19a198c294d9b0311f9d3fb6c9cd496beb
asakhacker-source <asakhacker-source@users.noreply.github.com>
2026-08-26 11:26:55 +0530

    Publish future technologies blog article

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'asark-app-v33';
+const CACHE_NAME = 'asark-app-v34';
 const OFFLINE_URL = './offline.html';
 const APP_SHELL = [
   './', './index.html', './architecture.html', './ancient.html', './modern.html', './futuristic.html', './technology.html', './ai-technology.html', './space.html', './semiconductor.html', './vlsi.html', './processor.html', './graphics-card.html',
